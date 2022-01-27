ff33b1ed67167f02af5bc45c6b18c3a5fc51c3b8
andreas <andreas.oktafiann@gmail.com>
2022-01-27 14:21:29 +0700

    TUGAS JS 19

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+function ulang() {
+  var text1 = "Semangat . . .";
+  console.log(text1.repeat(5));
+  console.log("=".repeat(30));
+
+  var text2 = "*****";
+  console.log(text2);
+  console.log(text2.repeat(5));
+  console.log(text2.repeat(10));
+  console.log(text2.repeat(15));
+  console.log(text2.repeat(20));
+  console.log(text2.repeat(15));
+  console.log(text2.repeat(10));
+  console.log(text2.repeat(5));
+  console.log(text2);
+
+  return;
+}
+
+ulang();
